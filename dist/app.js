b6f5126906c1a0d224ab8af5267d82c26d7ec3cb
areel007 <morenikejisunday@gmail.com>
2024-07-04 18:29:29 -0700

    spinning up the server (fix)

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const app = (0, express_1.default)();
-app.use("/", (req, res) => {
-    res.status(200).json({
-        message: "hello world",
-    });
-});
-app.listen(3000, () => console.log("server is running"));
+dotenv_1.default.config({ path: "./config.env" });
+// Configure CORS
+// const allowedOrigins = [
+//   "http://localhost:5174",
+//   "https://r28.ng",
+//   "https://r28.e37digital.com/",
+//   "http://r28.e37digital.com/",
+// ];
+// const corsOptions: cors.CorsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
+// app.use(cors(corsOptions));
+app.use((0, cors_1.default)());
+// Middleware
+if (process.env.NODE_ENV === "development") {
+    app.use((0, morgan_1.default)("dev"));
+}
+app.use(express_1.default.json());
+// Handle preflight requests
+// app.options("*", cors(corsOptions));
+exports.default = app;
