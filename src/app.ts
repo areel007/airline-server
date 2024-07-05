15d724531e0ec9efc5abffabfc9659cc6ca5591c
areel007 <morenikejisunday@gmail.com>
2024-07-04 18:05:08 -0700

    spinning up the server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "hello world",
+  });
+});
+
+app.listen(3000, () => console.log("server is running"));
